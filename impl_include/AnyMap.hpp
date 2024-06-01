e48d13193d0336c0cc3dfe0b3a9a0a8d230a2f2c
David Bruce <david.e.bruce@icloud.com>
2024-06-01 13:08:27 -0400

    before making a ProtobufSerializerApi

--- impl_include/AnyMap.hpp
+++ impl_include/AnyMap.hpp
@@ -7,15 +7,23 @@
 #include <ostream>
 #include <google/protobuf/message.h>
 
-using AnyMap = std::map<std::string, std::any>;
+namespace UpAbstractTransport
+{
+    using AnyMap = std::map<std::string, std::any>;
 
-AnyMap protobuf2anymap(const google::protobuf::Message&, bool get_options = false);
+    AnyMap protobuf2anymap(const google::protobuf::Message &, bool get_options = false);
 
-class Anymap2Protobuf {
-public:
-    Anymap2Protobuf(const AnyMap& am, google::protobuf::Message&);
-    bool is_valid();
-private:
-    struct Impl;
-    std::shared_ptr<Impl>   pImpl;
+    class Anymap2Protobuf
+    {
+    public:
+        Anymap2Protobuf(const AnyMap &am, google::protobuf::Message &);
+        bool is_valid();
+
+    private:
+        struct Impl;
+        std::shared_ptr<Impl> pImpl;
+    };
+
+    // This is exposed to the client API so cannot be declared here with defaults.
+    // void anymapFormat(std::ostream &os, const AnyMap &m, const std::string &pad = std::string("    "), size_t depth = 0, bool with_type = false);
 };
