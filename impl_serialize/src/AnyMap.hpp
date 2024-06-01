2dee16894ebe3603c508f1f5acd792c942006df9
David Bruce <david.e.bruce@icloud.com>
2024-06-01 11:31:24 -0400

    before attempting to share AnyMap

---
+++ impl_serialize/src/AnyMap.hpp
@@ -0,0 +1,21 @@
+#pragma once
+
+#include <map>
+#include <string>
+#include <any>
+#include <memory>
+#include <ostream>
+#include <google/protobuf/message.h>
+
+using AnyMap = std::map<std::string, std::any>;
+
+AnyMap protobuf2anymap(const google::protobuf::Message&, bool get_options = false);
+
+class Anymap2Protobuf {
+public:
+    Anymap2Protobuf(const AnyMap& am, google::protobuf::Message&);
+    bool is_valid();
+private:
+    struct Impl;
+    std::shared_ptr<Impl>   pImpl;
+};
