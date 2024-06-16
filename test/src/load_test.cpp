3cac27a9d7e73c3ce88eb3b12912e79fcce7717b
David Bruce <david.e.bruce@icloud.com>
2024-06-16 14:58:39 -0700

    reformat all of test/src

--- test/src/load_test.cpp
+++ test/src/load_test.cpp
@@ -1,29 +1,29 @@
-#include "UpAbstractTransport.h"
-#include <iostream>
 #include <unistd.h>
+
+#include <iostream>
+
+#include "UpAbstractTransport.h"
 
 using namespace std;
 using namespace UpAbstractTransport;
 
 auto init_doc =
-R"(
+    R"(
 {
     "implementation": "${IMPL_ZENOH}",
     "serializers": "${IMPL_SERIALIZE}"
 }
 )";
 
-int main(int argc, char* argv[])
-{
-    {
-        cout << "before load" << endl;
-        auto transport = Transport(init_doc);
-        cout << "after load" << endl;
-        auto concepts = transport.listConcepts();
-        for(const auto k : concepts) {
-            cout << "    concept: " << k << endl;
-        }
-    }
-    cout << "after unload" << endl;
+int main(int argc, char* argv[]) {
+	{
+		cout << "before load" << endl;
+		auto transport = Transport(init_doc);
+		cout << "after load" << endl;
+		auto concepts = transport.listConcepts();
+		for (const auto k : concepts) {
+			cout << "    concept: " << k << endl;
+		}
+	}
+	cout << "after unload" << endl;
 }
-
--- test/src/pub_test.cpp
+++ test/src/pub_test.cpp
@@ -1,6 +1,8 @@
-#include "UpAbstractTransport.h"
-#include <iostream>
 #include <unistd.h>
+
+#include <iostream>
+
+#include "UpAbstractTransport.h"
 
 using namespace std;
 using namespace UpAbstractTransport;
@@ -15,7 +17,7 @@ R"(
 )";
 #else
 auto init_doc =
-R"(
+    R"(
 {
     "implementation": "${IMPL_ZENOH}"
 }
@@ -23,25 +25,22 @@ R"(
 #endif
 
 template <class... Args>
-string genString(const char* fmt, Args... args)
-{
-    char buf[128];
-    snprintf(buf, sizeof(buf), fmt, args...);
-    return string(buf);
+string genString(const char* fmt, Args... args) {
+	char buf[128];
+	snprintf(buf, sizeof(buf), fmt, args...);
+	return string(buf);
 }
 
-int main(int argc, char* argv[])
-{
-    auto transport = Transport(init_doc);
+int main(int argc, char* argv[]) {
+	auto transport = Transport(init_doc);
 
-    auto p1 = Publisher(transport, "upl/p1");
-    auto p2 = Publisher(transport, "upl/p2");
+	auto p1 = Publisher(transport, "upl/p1");
+	auto p2 = Publisher(transport, "upl/p2");
 
-    for (auto i = 0; i < 5; i++) {
-        cout << endl << "client code pubishing " << i << endl;
-        p1(Message{genString("pay_A_%d", i), genString("attr_A_%d", i)});
-        p2(Message{genString("pay_B_%d", i), genString("attr_B_%d", i)});
-        usleep(100000);
-    }
+	for (auto i = 0; i < 5; i++) {
+		cout << endl << "client code pubishing " << i << endl;
+		p1(Message{genString("pay_A_%d", i), genString("attr_A_%d", i)});
+		p2(Message{genString("pay_B_%d", i), genString("attr_B_%d", i)});
+		usleep(100000);
+	}
 }
-
