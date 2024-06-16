4beca559e623e71fe879e25687dbd92181b14035
David Bruce <david.e.bruce@icloud.com>
2024-06-16 15:07:16 -0700

    reformat lib/src

--- lib/src/Serializer.cpp
+++ lib/src/Serializer.cpp
@@ -1,86 +1,59 @@
+#include "AnyMap.h"
 #include "HiddenTransport.h"
 #include "ProtobufSerializerApi.h"
-#include "AnyMap.h"
 
-namespace UpAbstractTransport
-{
-    using namespace std;
+namespace UpAbstractTransport {
+using namespace std;
 
-    string Serializer::messageName() const
-    {
-        return pImpl->messageName();
-    }
+string Serializer::messageName() const { return pImpl->messageName(); }
 
-    string Serializer::debugString() const
-    {
-        return pImpl->debugString();
-    }
+string Serializer::debugString() const { return pImpl->debugString(); }
 
-    string Serializer::serialize() const
-    {
-        return pImpl->serialize();
-    }
+string Serializer::serialize() const { return pImpl->serialize(); }
 
-    bool Serializer::deserialize(const std::string &arg)
-    {
-        return pImpl->deserialize(arg);
-    }
+bool Serializer::deserialize(const std::string& arg) {
+	return pImpl->deserialize(arg);
+}
 
-    bool Serializer::assign(const AnyMap &arg)
-    {
-        return pImpl->assign(arg);
-    }
+bool Serializer::assign(const AnyMap& arg) { return pImpl->assign(arg); }
 
-    AnyMap Serializer::fetch(bool describe) const
-    {
-        return pImpl->fetch(describe);
-    }
+AnyMap Serializer::fetch(bool describe) const { return pImpl->fetch(describe); }
 
-    /////////////////
+/////////////////
 
-    ProtobufSerializerApi::~ProtobufSerializerApi()
-    {
-        delete msg_ptr;
-    }
+ProtobufSerializerApi::~ProtobufSerializerApi() { delete msg_ptr; }
 
-    string ProtobufSerializerApi::messageName() const
-    {
-        return msg_ptr->GetTypeName();
-    }
+string ProtobufSerializerApi::messageName() const {
+	return msg_ptr->GetTypeName();
+}
 
-    string ProtobufSerializerApi::debugString() const
-    {
-        return msg_ptr->DebugString();
-    }
+string ProtobufSerializerApi::debugString() const {
+	return msg_ptr->DebugString();
+}
 
-    string ProtobufSerializerApi::validate() const
-    {
-        return string(); // default is to validate, non-empty string means good
-    }
+string ProtobufSerializerApi::validate() const {
+	return string();  // default is to validate, non-empty string means good
+}
 
-    string ProtobufSerializerApi::serialize() const
-    {
-        string v = validate();
-        if (v.size() > 0) {
-            throw runtime_error(v);
-        }
-        return msg_ptr->SerializeAsString();
-    }
+string ProtobufSerializerApi::serialize() const {
+	string v = validate();
+	if (v.size() > 0) {
+		throw runtime_error(v);
+	}
+	return msg_ptr->SerializeAsString();
+}
 
-    bool ProtobufSerializerApi::deserialize(const string & arg)
-    {
-        return msg_ptr->ParseFromString(arg);
-    }
+bool ProtobufSerializerApi::deserialize(const string& arg) {
+	return msg_ptr->ParseFromString(arg);
+}
 
-    bool ProtobufSerializerApi::assign(const AnyMap &arg)
-    {
-        Anymap2Protobuf s(arg, *msg_ptr);
-        return s.is_valid();
-    }
+bool ProtobufSerializerApi::assign(const AnyMap& arg) {
+	Anymap2Protobuf s(arg, *msg_ptr);
+	return s.is_valid();
+}
 
-    AnyMap ProtobufSerializerApi::fetch(bool describe) const
-    {
-        return protobuf2anymap(*msg_ptr, describe);
-    }
+AnyMap ProtobufSerializerApi::fetch(bool describe) const {
+	return protobuf2anymap(*msg_ptr, describe);
+}
 
-}; // namespace UpAbstractTransport
+};  // namespace UpAbstractTransport
--- lib/src/TransportBase.cpp
+++ lib/src/TransportBase.cpp
@@ -1,74 +1,66 @@
-#include "HiddenTransport.h"
-#include <map>
+#include <wordexp.h>
+
 #include <any>
 #include <filesystem>
 #include <iostream>
+#include <map>
 #include <sstream>
-#include <wordexp.h>
 
-namespace UpAbstractTransport
-{
-    using namespace std;
+#include "HiddenTransport.h"
 
-    string resolve_path(const string &impl)
-    {
-        wordexp_t we;
-        auto retval = wordexp(impl.c_str(), &we, WRDE_UNDEF);
-        if (retval != 0)
-        {
-            stringstream ss;
-            ss << "wordexp expansion failed on \"" << impl << '"';
-            throw runtime_error(ss.str());
-        }
-        if (we.we_wordc != 1)
-        {
-            wordfree(&we);
-            stringstream ss;
-            ss << "wordexp expanded to more than one path on \"" << impl << '"';
-            throw runtime_error(ss.str());
-        }
-        string result(we.we_wordv[0]);
-        wordfree(&we);
-        return result;
-    }
+namespace UpAbstractTransport {
+using namespace std;
 
-    HiddenTransport::HiddenTransport(const Doc &init_doc)
-    {
-        string path;
-        path = resolve_path(init_doc["implementation"].get<string>());
-        conceptPlugin = FactoryPlugin<ConceptFactories>(path);
-        conceptImpl = conceptPlugin->getImplementation(init_doc);
+string resolve_path(const string& impl) {
+	wordexp_t we;
+	auto retval = wordexp(impl.c_str(), &we, WRDE_UNDEF);
+	if (retval != 0) {
+		stringstream ss;
+		ss << "wordexp expansion failed on \"" << impl << '"';
+		throw runtime_error(ss.str());
+	}
+	if (we.we_wordc != 1) {
+		wordfree(&we);
+		stringstream ss;
+		ss << "wordexp expanded to more than one path on \"" << impl << '"';
+		throw runtime_error(ss.str());
+	}
+	string result(we.we_wordv[0]);
+	wordfree(&we);
+	return result;
+}
 
-        if (init_doc.contains("serializers")) {
-            path = resolve_path(init_doc["serializers"].get<string>());
-            serialPlugin = FactoryPlugin<SerializerFactories>(path);
-        }
-    }
+HiddenTransport::HiddenTransport(const Doc& init_doc) {
+	string path;
+	path = resolve_path(init_doc["implementation"].get<string>());
+	conceptPlugin = FactoryPlugin<ConceptFactories>(path);
+	conceptImpl = conceptPlugin->getImplementation(init_doc);
 
-    Transport::Transport(const Doc &init_doc) : pImpl(new HiddenTransport(init_doc))
-    {
-    }
+	if (init_doc.contains("serializers")) {
+		path = resolve_path(init_doc["serializers"].get<string>());
+		serialPlugin = FactoryPlugin<SerializerFactories>(path);
+	}
+}
 
-    Transport::Transport(const char *init_string) : Transport(Doc::parse(init_string))
-    {
-    }
+Transport::Transport(const Doc& init_doc)
+    : pImpl(new HiddenTransport(init_doc)) {}
 
-    any Transport::getConcept(const string &name)
-    {
-        return pImpl->conceptImpl->getConcept(name);
-    }
+Transport::Transport(const char* init_string)
+    : Transport(Doc::parse(init_string)) {}
 
-    vector<string> Transport::listConcepts()
-    {
-        return pImpl->conceptImpl->listConcepts();
-    }
+any Transport::getConcept(const string& name) {
+	return pImpl->conceptImpl->getConcept(name);
+}
 
-    Serializer Transport::getSerializer(const string &name)
-    {
-        Serializer ret;
-        
-        ret.pImpl = pImpl->serialPlugin->get_instance(name);
-        return ret;
-    }
+vector<string> Transport::listConcepts() {
+	return pImpl->conceptImpl->listConcepts();
+}
 
-}; // namespace UpAbstractTransport
+Serializer Transport::getSerializer(const string& name) {
+	Serializer ret;
+
+	ret.pImpl = pImpl->serialPlugin->get_instance(name);
+	return ret;
+}
+
+};  // namespace UpAbstractTransport
