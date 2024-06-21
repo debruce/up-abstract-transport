b93827646e9536010302a0c8f79f6efbb75d5b4a
David Bruce <david.e.bruce@icloud.com>
2024-06-21 15:33:20 -0400

    fix sub_test attributes deserialization

--- test/src/sub_test.cpp
+++ test/src/sub_test.cpp
@@ -18,12 +18,14 @@ auto init_doc =
 
 int main(int argc, char* argv[]) {
 	auto transport = Transport(init_doc);
-	auto callback = [](const string& sending_topic,
+	auto callback = [&](const string& sending_topic,
 	                   const string& listening_topic, const Message& message) {
+		auto attributes = transport.getSerializer("UAttributes");
+		attributes.deserialize(message.attributes);
 		cout << "subscriber callback with"
 		     << " from=" << sending_topic << " to=" << listening_topic
 		     << " payload=" << message.payload
-		     << " attributes=" << message.attributes << endl;
+		     << " attributes=" << attributes.debugString() << endl;
 	};
 	auto subscriber = Subscriber(transport, "upl/*", callback, "Zenoh");
 	sleep(10000);
